4afdb3c552babe56e8c63c3ad12c6cec00eadfee
evertongr <evertongusmaorocha@gmail.com>
2023-02-21 15:53:53 -0300

    Version 4 - Include Welcome4

---
+++ src/Welcome4.java
@@ -0,0 +1,15 @@
+// Picture 2.3: Welcome2.java
+// Print a text line with many instruction
+
+/* Object: program to print message in many lines using printf
+   Developer: Everton
+   Develop in 02.21.2023 03:53 PM
+*/
+public class Welcome4
+{
+    // method main begin the execution of the app Java
+    public static void main(String[] args)
+    {
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } // end the method
+} // end the main
