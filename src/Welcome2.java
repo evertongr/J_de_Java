fdba4f3b0ee259aade00edf07abbada92d0b1d3f
evertongr <evertongusmaorocha@gmail.com>
2023-02-21 15:31:18 -0300

    Version 1 - Include Welcome2

---
+++ src/Welcome2.java
@@ -0,0 +1,11 @@
+// Picture 2.3: Welcome2.java
+// Print a text line with many instruction
+public class Welcome2
+{
+    // method main begin the execution of the app Java
+    public static void main(String[] args)
+    {
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    } // end the method
+} // end the main
