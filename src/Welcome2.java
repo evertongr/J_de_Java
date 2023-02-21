8c097255823611115067650da6c3bebdbdab1158
evertongr <evertongusmaorocha@gmail.com>
2023-02-21 15:34:23 -0300

    Version 2 - Include Welcome2

--- src/Welcome2.java
+++ src/Welcome2.java
@@ -1,5 +1,10 @@
 // Picture 2.3: Welcome2.java
 // Print a text line with many instruction
+
+/* Object: program to print message with print and println
+   Developer: Everton
+   Develop in 02.21.2023 03:33 PM
+*/
 public class Welcome2
 {
     // method main begin the execution of the app Java
