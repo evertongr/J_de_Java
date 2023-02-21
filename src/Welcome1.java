359d3e6391610ad5460f643a8ba2122d3a1a3c16
evertongr <evertongusmaorocha@gmail.com>
2023-02-21 14:58:46 -0300

    Version 1

---
+++ src/Welcome1.java
@@ -0,0 +1,18 @@
+// Figura 2.1: Welcome1.java
+// Programa de impressão de texto
+
+/* Object: Print the message of screen
+   Develop by Everton
+   Develop in 02.21.2023 02?38 PM
+ */
+
+/* This is a traditional comment.
+This can be divided in many lines */
+
+//** This is a Javadoc comment. This is utility for the documentation of program. */
+public class Welcome1 {
+    // method main begin the execution of app Java
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    } // end of method main
+}// end of class Welcome1
