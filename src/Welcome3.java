346743e5ce5f6364c3b6dbbdc32c7784a0a69317
evertongr <evertongusmaorocha@gmail.com>
2023-02-21 15:39:33 -0300

    Version 3 - Include Welcome3

---
+++ src/Welcome3.java
@@ -0,0 +1,15 @@
+// Picture 2.3: Welcome2.java
+// Print a text line with many instruction
+
+/* Object: program to print message in many lines using \n
+   Developer: Everton
+   Develop in 02.21.2023 03:39 PM
+*/
+public class Welcome3
+{
+    // method main begin the execution of the app Java
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+    } // end the method
+} // end the main
